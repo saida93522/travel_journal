6803d84b393bf0aef9af131e1fc1e726ed8c002b
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-27 19:13:35 -0500

    commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <img src="nav_logo.svg" alt="red marker" />
+      <span class="nav-title">my travel journal.</span>
+    </nav>
+  );
+};
+
+export default Nav;
